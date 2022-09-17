885b511a91bfc655013eb9c2c45753cc452d45e7
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-09-17 10:53:10 +0600

    case

--- case.js
+++ case.js
@@ -1,2 +1,8 @@
 const lyrics = 'tumi bondhu kala pakhi ami jeno ki , bonsonto kale tomay bolte pari ni '
 
+const searchString='Pakhi';
+// const doesExist= lyrics.includes(searchString);
+const lyricsLowerCase = lyrics.toLowerCase();
+const searchStringLower = searchString.toLowerCase();
+const doesExist= lyricsLowerCase.includes(searchStringLower);
+console.log(doesExist);
